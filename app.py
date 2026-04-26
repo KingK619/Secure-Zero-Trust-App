4763a129719c5219ba29e8373079bd1a3542725d
KV <abc@gmail.com>
2026-04-26 16:54:20 +0530

    db

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ def health():
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASS"),
             dbname=os.getenv("DB_NAME"),
+            port=os.getenv("DB_PORT", "5432"),
             sslmode="require"
         )
         conn.close()
